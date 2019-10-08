1852f94ab5a2d2795752f7b99366cc0b98a63157
drmeloy <drmeloy@gmail.com>
2019-10-07 18:06:11 -0600

    figured out first part of problem 6

--- src/math-functions.js
+++ src/math-functions.js
@@ -134,7 +134,16 @@ IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To
 This function should be dynamic, accepting an array of any length.
 */
 
-export function multiplyAnyArray(dynamicArray) { //eslint-disable-line
+export function multiplyAnyArray(dynamicArray) {
+    let multTotal = 1;
+    
+    for(let i = 0; i < dynamicArray.length; i++){
+        multTotal = multiply(multTotal, dynamicArray[i])[0];
+    }
+    
+    
+
+    return [multTotal, `The numbers ${dynamicArray.foreach} have a product of ${multTotal}.`];
 
 }
 
